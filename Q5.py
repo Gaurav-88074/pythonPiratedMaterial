4ace8f29d834501c4b2ec77f92192a3d5e38d8a0
Gaurav-88074 <81903385+Gaurav-88074@users.noreply.github.com>
2021-11-16 17:15:04 +0530

    FIX:Changes in file name

--- Q5.py
+++ Q5.py
@@ -3,8 +3,11 @@ def compute(x):
     res =1
     chk =1
     for i in range(2,x+1,2):
-        res+=(i/m(i))*((-1)**i)
-       
+        if chk%2==0:
+            res+=(i/m(i))
+        else:
+            res-=(i/m(i))
+        chk+=1
     return res
 if __name__ == '__main__':
     n  =int(input("Enter the value of n : "))
