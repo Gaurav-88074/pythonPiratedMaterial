9f8a4a1da4942047f7cc0f15773aa52eab10fea3
Gaurav-88074 <81903385+Gaurav-88074@users.noreply.github.com>
2021-11-16 11:55:17 +0530

    fix question4

--- 4.py
+++ 4.py
@@ -1,7 +1,9 @@
-def computeDigit(n):
+def computeDigit(n,s):
     if n==0:
-        return 0
+        return s
     else:
-        return computeDigit(n//10)+1
+        s.add(n%10)
+        return computeDigit(n//10,s)
+Set = set()
 userInput = int(input("Enter the number : "))
-print(f"{computeDigit(userInput)} digits present")
+print(f"{computeDigit(userInput,Set)}")
